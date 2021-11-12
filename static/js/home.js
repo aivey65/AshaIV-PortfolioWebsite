a2afc52a6c41792cebf0829f5030f5df2d476acd
Asha Ivey <ashavivey@gmail.com>
2021-11-11 18:05:03 -0600

    Updated project style. Also added resume and contact sections. Ready for a first launch.

--- static/js/home.js
+++ static/js/home.js
@@ -17,7 +17,7 @@ function loadProjects(projectsData) {
     const projectsContainer = document.getElementById('projects-container');
 
     for (const project of projectsData) {
-        const name = document.createElement('h2');
+        const name = document.createElement('h3');
         name.innerHTML = project.name;
 
         const year = document.createElement('h4');
@@ -32,22 +32,29 @@ function loadProjects(projectsData) {
             git.href = project.github;
         }
 
+        const breakpoint = document.createElement('br');
+
         const live = document.createElement('a')
         if (project.live != "") {
             live.innerHTML = "See it Live";
             live.href = project.live;
         }
 
+        const skillHead = document.createElement('h4');
+        skillHead.innerHTML = "Skills";
+
         const skills = document.createElement('div');
         for (const skill of project.skills) {
-            const s = document.createElement('h3');
+            const s = document.createElement('p');
             s.innerHTML = skill;
             skills.appendChild(s);
+            
         }
-
+        skills.classList.add('skill-div');
+        
         projectDiv = document.createElement('div');
-        projectDiv.classList.add('projects');
-        projectDiv.append(name, year, des, git, live, skills);
+        projectDiv.classList.add('project-card');
+        projectDiv.append(name, year, des, git, breakpoint, live, skillHead, skills);
         
         projectsContainer.appendChild(projectDiv);
     }
@@ -77,4 +84,12 @@ function resumeAction() {
 function contactAction() {
     document.getElementById('contact').scrollIntoView();
 
+}
+
+////////////////////
+// Other Functions//
+////////////////////
+function copyEmail() {
+    var email = document.getElementById('email-text');
+    navigator.clipboard.writeText(email.innerHTML);
 }
