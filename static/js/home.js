b9ebafb571c3e3f3d53245b5d07b52278ee3c207
Asha Ivey <ashavivey@gmail.com>
2022-06-08 00:00:43 -0500

    Styles, background, paralax, bluring in contact section, uploaded new resume, etc.

--- static/js/home.js
+++ static/js/home.js
@@ -97,3 +97,15 @@ function copyEmail() {
 function showMoreProjectInfo(projectID) {
     location.href = '/project/' + projectID
 }
+
+
+////////////////////
+// Scroll Paralax //
+////////////////////
+// Paralax scrolling method adapted from https://stackoverflow.com/questions/29240028/css-make-a-background-image-scroll-slower-than-everything-else
+window.addEventListener('scroll', () => {
+    const scrolltotop = window.pageYOffset;
+    var factor = 0.5;
+    var yvalue = scrolltotop * factor;
+    document.body.style.backgroundPosition = "center " + yvalue + "px";
+});
