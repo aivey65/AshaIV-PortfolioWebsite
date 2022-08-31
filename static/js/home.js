733898add30283885e9516e0d16e17c10d280bf0
Asha Ivey <ashavivey@gmail.com>
2022-08-31 18:20:52 -0500

    I think final fix for home section down arrow

--- static/js/home.js
+++ static/js/home.js
@@ -198,4 +198,9 @@ window.addEventListener('scroll', () => {
 // (to account for mobile device interface) //
 //////////////////////////////////////////////
 let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', vh + 'px')
+document.documentElement.style.setProperty('--vh', vh + 'px');
+
+window.addEventListener('resize', () => {
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', vh + 'px');
+});
